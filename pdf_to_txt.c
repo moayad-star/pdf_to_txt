de41128bec4998b0f899721fb05a8f779c071b6f
Moayad Ahmed <60769512+moayad-star@users.noreply.github.com>
2022-12-05 17:04:32 +0300

    c language

---
+++ pdf_to_txt.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+int man(){
+  printf("The program is under development \nfor the purpose of supporting \nthe c programming language");
+  printf("\n\nthanks for your support");
+  return 0
+}
